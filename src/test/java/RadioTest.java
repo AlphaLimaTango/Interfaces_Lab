f8690ea0502547914122d435f8d9732e6b62f7a0
sierratango34 <k.sritawan@gmail.com>
2019-05-14 13:19:09 +0100

    radio test

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,26 @@
+import components.Component;
+import components.Radio;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RadioTest {
+
+    Radio radio;
+
+    @Before
+    public void before(){
+        radio = new Radio(Component.Make.SONY, Component.Model.TDD);
+    }
+
+    @Test
+    public void canGetMake(){
+        assertEquals(Component.Make.SONY, radio.getMake());
+    }
+
+    @Test
+    public void canGetModel() {
+        assertEquals(Component.Model.TDD, radio.getModel());
+    }
 }
